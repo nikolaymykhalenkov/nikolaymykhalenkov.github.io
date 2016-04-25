c5a62de649737026641ba044231c43c435a7921a
Nikolay <nikkomyk@gmail.com>
2016-04-26 00:40:50 +0300

    majore improvements

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	var colors = ["orange", "green", "blue"]; 
 	var index = Math.floor(Math.random() * colors.length);
 	$("body").addClass(colors[index]);
+
+	$("#tab1").click(function() {
+		$("#tab2-c").hide();
+		$("#tab1-c").show();
+	});
+	$("#tab2").click(function() {
+		$("#tab1-c").hide();
+		$("#tab2-c").show();
+	});
+
 });
