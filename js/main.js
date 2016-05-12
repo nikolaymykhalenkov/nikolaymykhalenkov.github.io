9c7cfda65520445b4b26fc3e8b5faf9348e26ab4
Nikolay <nikkomyk@gmail.com>
2016-05-13 00:42:39 +0300

    Links changed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		$("#content-tab").append("<div class='content-item'><h2>About me</h2><p>I am student from Ukraine. My passion is programming, web-development and game-development, science and space.</p></div>");
 	});
 	$("#menu-item-2").click(function() {
-		$("#content-tab").append("<div class='content-item'><h2>Portfolio</h2><p>My last work was <a href='http://render-room.com'>website</a> for guys from Render Room. Now I`m working on <a href='http://bolgov.kiev.ua'>website</a> for surgeon Michael Bolgov.</p><p>Also, now I`m working on a game using Unity 3D and C#.</p></div>");
+		$("#content-tab").append("<div class='content-item'><h2>Portfolio</h2><p>My last work was <a href='http://www.render-room.com'>website</a> for guys from Render Room. Now I`m working on <a href='http://www.bolgov.kiev.ua'>website</a> for surgeon Michael Bolgov.</p><p>Also, now I`m working on a game using Unity 3D and C#.</p></div>");
 	});
 	$("#menu-item-3").click(function() {
 		$("#content-tab").append("<div class='content-item'><h2>Contacts</h2><p>Feel free to mail me on <a href='mailto:nikkomyk@gmail.com'>nikkomyk@gmail.com</a></p><p>If you want - contact me on <a href='https://ua.linkedin.com/in/nikolay-mykhalenkov-95797495'>LindekIn</a> or <a href='https://new.vk.com/johndrakonwilson'>VK.com</a>.</p></div>");
